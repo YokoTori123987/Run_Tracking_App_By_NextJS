2c54bd60ca8eec4b7ca3a3e63f50879df74863f4
YokoTori123987 <nongtaop1@gmail.com>
2023-01-18 16:39:14 +0700

    update Data

--- run-tracking-next/src/pages/_app.js
+++ run-tracking-next/src/pages/_app.js
@@ -1,5 +1,20 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "../styles/header.css";
+// import "../styles/index.css";
+import { Row, Col } from "antd";
+import { Breadcrumb, Layout, Menu, theme } from "antd";
 
+const { Header, Content, Sider } = Layout;
+import NavbarComponents from "@/components/NavbarComponents";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Layout>
+        <NavbarComponents />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Layout>
+    </div>
+  );
 }
