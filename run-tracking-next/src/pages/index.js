23fc21c7c361d290527d4664b339643ed746dc4d
YokoTori123987 <nongtaop1@gmail.com>
2023-02-08 10:46:43 +0700

    edit condition homepage

--- run-tracking-next/src/pages/index.js
+++ run-tracking-next/src/pages/index.js
@@ -9,58 +9,22 @@ import HomeGoverned from "./HomeMain/HomeGoverned";
 import HomeAdmin from "./HomeMain/HomeAdmin";
 export default function Home() {
   const { user } = useUserAuth();
-  console.log(user);
-  return (
-    <>
-      {user ? (
-        <>
-          {user.role === "USER" ? (
-            <>
-              <HomeUser />
-            </>
-          ) : (
-            <>
-              {user.role === "SCANNER" ? (
-                <>
-                  <HomeScanner />
-                </>
-              ) : (
-                <>
-                  {user.role === "OWNED" ? (
-                    <>
-                      <HomeOwned />
-                    </>
-                  ) : (
-                    <>
-                      {user.role === "GOVERNED" ? (
-                        <>
-                          <HomeGoverned />
-                        </>
-                      ) : (
-                        <>
-                          {user.role === "ADMIN" ? (
-                            <>
-                              <HomeAdmin />
-                            </>
-                          ) : (
-                            <>
-                              <HomeGlobal />
-                            </>
-                          )}
-                        </>
-                      )}
-                    </>
-                  )}
-                </>
-              )}
-            </>
-          )}
-        </>
-      ) : (
-        <>
-          <HomeGlobal />
-        </>
-      )}
-    </>
-  );
+  if (user) {
+    if (user.role === "USER") {
+      return <HomeUser />;
+    }
+    if (user.role === "SCANNER") {
+      return <HomeScanner />;
+    }
+    if (user.role === "OWNED") {
+      return <HomeOwned />;
+    }
+    if (user.role === "GOVERNED") {
+      return <HomeGoverned />;
+    }
+    if (user.role === "ADMIN") {
+      return <HomeAdmin />;
+    }
+  }
+  return <HomeGlobal />;
 }
