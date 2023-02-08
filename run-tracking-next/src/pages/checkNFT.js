eee88977c135935f95c60936bd31cf7f837bfb09
YokoTori123987 <nongtaop1@gmail.com>
2023-02-08 12:52:03 +0700

    add CheckNFT Test

---
+++ run-tracking-next/src/pages/checkNFT.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Button } from "antd";
+export default function CheckNFT() {
+  const scanButton = addEventListener("click", async () => {
+    log("User clicked scan button");
+    try {
+      const ndef = new NDEFReader();
+      await ndef.scan();
+      log("> Scan started");
+
+      ndef.addEventListener("readingerror", () => {
+        log("Argh! Cannot read data from the NFC tag. Try another one?");
+      });
+
+      ndef.addEventListener("reading", ({ message, serialNumber }) => {
+        log(`> Serial Number: ${serialNumber}`);
+        log(`> Records: (${message.records.length})`);
+      });
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  });
+
+  //   writeButton.addEventListener("click", async () => {
+  //     log("User clicked write button");
+
+  //     try {
+  //       const ndef = new NDEFReader();
+  //       await ndef.write("Hello world!");
+  //       log("> Message written");
+  //     } catch (error) {
+  //       log("Argh! " + error);
+  //     }
+  //   });
+
+  //   makeReadOnlyButton.addEventListener("click", async () => {
+  //     log("User clicked make read-only button");
+
+  //     try {
+  //       const ndef = new NDEFReader();
+  //       await ndef.makeReadOnly();
+  //       log("> NFC tag has been made permanently read-only");
+  //     } catch (error) {
+  //       log("Argh! " + error);
+  //     }
+  //   });
+  return (
+    <>
+      <Button onClick={scanButton()}>Scan</Button>
+    </>
+  );
+}
